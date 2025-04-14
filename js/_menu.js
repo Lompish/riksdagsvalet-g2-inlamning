c261cc6e54b439273e566398ddfab7d413490c37
Malin Hööglund <malin0hoog@gmail.com>
2025-04-14 13:49:56 +0200

    changed name on tab pages, added one more js file for Nadia

--- js/_menu.js
+++ js/_menu.js
@@ -1,6 +1,7 @@
-createMenu('Statistics Template JS', [
-  { name: 'Nytt i version 7', script: 'new-in-v7.js' },
-  { name: 'Visa ett år', script: 'one-year.js' },
-  { name: 'Jämför två år', script: 'compare-two-years.js' },
-  { name: 'Hitta trender', script: 'trends.js' }
+createMenu('G2 Gruppuppgift - Sveriges Riksval 2018 och 2022', [
+  { name: 'Startsida', script: 'startingPage.js' },
+  { name: 'Sida 2', script: 'thomas.js' },
+  { name: 'Sida 3', script: 'linda.js' },
+  { name: 'Sida 4', script: 'malin.js' },
+  { name: 'Sida 5', script: 'nadia.js' }
 ]);
