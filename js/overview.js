39902458df306819908759e3b6567f52364f0a86
thomasavguld <rosen.thomas@gmail.com>
2025-04-15 12:59:22 +0200

    Added hypotheses to overview page

--- js/overview.js
+++ js/overview.js
@@ -7,9 +7,15 @@ import makeChartFriendly from './libs/makeChartFriendly.js';
 import * as vars from './commonVars.js';
 
 addMdToPage(`
-## Här står det grejer
-Olika typer av saker
+##Påverkar inkomstnivå politiska rörelsemönster i svenska kommuner? 
+
+Vi vill undersöka om det finns ett samband mellan förändring i inkomstnivå (medel- och medianinkomst) och förändringar i röstmönster för olika partier mellan valen 2018 och 2022, på kommunnivå. 
+
+**Hypoteser:**
+
+I de kommuner där inkomsterna ökat mest har partier inom tidö-samarbetet (M, Kd, L & SD) fått ett ökat antal röster mellan valen 2018-2022.  
+
+I de kommuner där inkomstökningen varit låg eller negativ har partierna i opposition (S, V, Mp och C) behållit eller ökat sitt stöd.
 <br>
 <br>
 `);
-
