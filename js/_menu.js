3cad4ec22e30774e59b3bbc087cb39224e82ac88
thomasavguld <rosen.thomas@gmail.com>
2025-04-16 13:26:33 +0200

    Updated menu

--- js/_menu.js
+++ js/_menu.js
@@ -2,5 +2,7 @@ createMenu('Inkomstnivå och valresultat i svenska kommuner', [
   { name: 'Översikt', script: 'overview.js' },
   { name: 'Sida ett', script: 'placeholder.js' },
   { name: 'Sida två', script: 'placeholder.js' },
-  { name: 'Sida tre', script: 'placeholder.js' }
+  { name: 'Sida tre', script: 'placeholder.js' },
+  { name: 'Sida fyra', script: 'placeholder.js' },
+  { name: 'Källor', script: 'placeholder.js' }
 ]);
