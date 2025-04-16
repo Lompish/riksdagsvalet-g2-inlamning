229f8f89cfa673413c94cc7ac8e055be9ed486e2
thomasavguld <rosen.thomas@gmail.com>
2025-04-16 13:24:26 +0200

    Added hypothesis to overview

--- js/overview.js
+++ js/overview.js
@@ -1,15 +1,16 @@
 addMdToPage(`
 ## Påverkar inkomstnivå politiska rörelsemönster i svenska kommuner?
 
-Vi vill undersöka om det finns ett samband mellan förändring i inkomstnivå (medel- och medianinkomst) och förändringar i röstmönster för olika partier mellan valen 2018 och 2022, på kommunnivå. Vi vill också veta om valdeltagandet påverkas av arbetslöshet. 
+Vi vill undersöka om det finns ett samband mellan förändring i inkomstnivå (medel- och medianinkomst) och förändringar i röstmönster för olika partier mellan valen 2018 och 2022, på kommunnivå. Vi vill också veta om valdeltagandet påverkas av variabler som arbetslöshet och utbildningsnivå. 
 
 **Hypoteser:**
 
-* I de kommuner där inkomsterna ökat mest har partier inom tidö-samarbetet (M, Kd, L & SD) fått ett ökat antal röster mellan valen 2018-2022.  
+1. I de kommuner där inkomsterna ökat mest har partier inom tidö-samarbetet (M, Kd, L & SD) fått ett ökat antal röster mellan valen 2018-2022.  
 
-* I de kommuner där inkomstökningen varit låg eller negativ har partierna i opposition (S, V, Mp och C) behållit eller ökat sitt stöd.
+2. I de kommuner där inkomstökningen varit låg eller negativ har partierna i opposition (S, V, Mp och C) behållit eller ökat sitt stöd.
 
-* Valdeltagandet är lägre i kommuner med hög arbetslöshet.
+3. Valdeltagandet är lägre i kommuner med hög arbetslöshet och högre i kommuner med lägre arbetslöshet.
 
-* 
+4. Valdeltagandet är lägre i kommuner med låg utbildningsnivå och högre i kommuner med hög utbildningsnivå.  
+ 
 `);
