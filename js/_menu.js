558ab4bf50236e46f3d41a7f51aa5c7d6c0444bb
Lompish <linda.hiermann.data24sto@edu.tucsweden.se>
2025-04-17 13:55:53 +0200

    Added new files to main

--- js/_menu.js
+++ js/_menu.js
@@ -1,8 +1,8 @@
 createMenu('Inkomstnivå och valresultat i svenska kommuner', [
   { name: 'Översikt', script: 'overview.js' },
-  { name: 'Sida ett', script: 'placeholder.js' },
-  { name: 'Sida två', script: 'placeholder.js' },
-  { name: 'Sida tre', script: 'placeholder.js' },
-  { name: 'Sida fyra', script: 'placeholder.js' },
-  { name: 'Källor', script: 'placeholder.js' }
+  { name: 'Sida ett', script: 'page1.js' },
+  { name: 'Sida två', script: 'page2.js' },
+  { name: 'Sida tre', script: 'page3.js' },
+  { name: 'Sida fyra', script: 'page4.js' },
+  { name: 'Sammanfattning & Resultat', script: 'end.js' }
 ]);
