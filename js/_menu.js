5de875d76f8befe7b50ba0376df1e8d4e542064b
thomasavguld <rosen.thomas@gmail.com>
2025-04-30 10:06:31 +0200

    Changed order of buttons

--- js/_menu.js
+++ js/_menu.js
@@ -1,7 +1,7 @@
 createMenu('Inkomstnivå och valresultat i svenska kommuner', [
   { name: 'Översikt', script: 'overview.js' },
-  { name: 'Nadia', script: 'page1.js' },
+  { name: 'Linda', script: 'page4.js' },
   { name: 'Malin', script: 'page2.js' },
   { name: 'Thomas', script: 'page3.js' },
-  { name: 'Linda', script: 'page4.js' }
+  { name: 'Nadia', script: 'page1.js' }
 ])
