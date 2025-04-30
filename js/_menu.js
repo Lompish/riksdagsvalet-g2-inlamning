c8cfd56b21630aece7c70861dd5ae1b08ec2352b
thomasavguld <rosen.thomas@gmail.com>
2025-04-30 10:04:12 +0200

    Changed menu buttons to group member names

--- js/_menu.js
+++ js/_menu.js
@@ -1,8 +1,7 @@
 createMenu('Inkomstnivå och valresultat i svenska kommuner', [
   { name: 'Översikt', script: 'overview.js' },
-  { name: 'Sida ett', script: 'page1.js' },
-  { name: 'Sida två', script: 'page2.js' },
-  { name: 'Sida tre', script: 'page3.js' },
-  { name: 'Sida fyra', script: 'page4.js' },
-  { name: 'Sammanfattning & Resultat', script: 'end.js' }
+  { name: 'Nadia', script: 'page1.js' },
+  { name: 'Malin', script: 'page2.js' },
+  { name: 'Thomas', script: 'page3.js' },
+  { name: 'Linda', script: 'page4.js' }
 ])
