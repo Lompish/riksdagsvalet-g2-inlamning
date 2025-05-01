14d13aafefbb941e837385c9264b2337ee608599
Lompish <linda.hiermann.data24sto@edu.tucsweden.se>
2025-05-01 12:12:23 +0200

    Added sources.js

---
+++ js/sources.js
@@ -0,0 +1,15 @@
+addMdToPage(`
+### Externa datakällor. 
+<br>
+**SCB** står för Statistiska centralbyrån, och är en svensk myndighet som samlar in, sammanställer och publicerar statistik om Sverige. 
+Den används av regeringen, forskare, företag och allmänheten för att få underlag till beslut och analyser.Exempel på områden SCB täcker är befolkning, ekonomi, utbildning och arbetsmarknad. 
+<br>
+  https://www.scb.se/hitta-statistik/sverige-i-siffror/utbildning-jobb-och-pengar/utbildningsnivan-i-sverige/ 
+  https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__ME__ME0105__ME0105C/ME0105T28/table/tableViewLayout1/ 
+  https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__ME__ME0105__ME0105C/ME0105T28/table/tableViewLayout1/ 
+<br>
+**Arbetsförmedlingen** är en svensk statlig myndighet som hjälper människor att hitta jobb och arbetsgivare att hitta personal.
+De erbjuder stöd till arbetssökande, till exempel hjälp med jobbsökande, praktikplatser och utbildningar, samt stöd till arbetsgivare vid rekrytering.
+<br>
+  https://arbetsformedlingen.se/statistik/sok-statistik/tidigare-statistik-tidsserier 
+`);
