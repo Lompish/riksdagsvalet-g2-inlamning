f6cd1a95e4a6e5c7a5a65d7ff04d96c40f1535ff
Lompish <linda.hiermann.data24sto@edu.tucsweden.se>
2025-05-01 12:50:47 +0200

    added markdown and links to sources.js

--- js/sources.js
+++ js/sources.js
@@ -1,15 +1,23 @@
 addMdToPage(`
 ### Externa datakällor. 
 <br>
-**SCB** står för Statistiska centralbyrån, och är en svensk myndighet som samlar in, sammanställer och publicerar statistik om Sverige. 
-Den används av regeringen, forskare, företag och allmänheten för att få underlag till beslut och analyser.Exempel på områden SCB täcker är befolkning, ekonomi, utbildning och arbetsmarknad. 
+
+SCB står för Statistiska centralbyrån, och är en svensk myndighet som samlar in, sammanställer och publicerar statistik om Sverige. 
+Den används av regeringen, forskare, företag och allmänheten för att få underlag till beslut och analyser. Exempel på områden SCB täcker är befolkning, ekonomi, utbildning och arbetsmarknad. 
 <br>
-  https://www.scb.se/hitta-statistik/sverige-i-siffror/utbildning-jobb-och-pengar/utbildningsnivan-i-sverige/ 
-  https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__ME__ME0105__ME0105C/ME0105T28/table/tableViewLayout1/ 
-  https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__ME__ME0105__ME0105C/ME0105T28/table/tableViewLayout1/ 
 <br>
-**Arbetsförmedlingen** är en svensk statlig myndighet som hjälper människor att hitta jobb och arbetsgivare att hitta personal.
+Utbildningsnivå: [https://www.scb.se/hitta-statistik/sverige-i-siffror/utbildning-jobb-och-pengar/utbildningsnivan-i-sverige/](https://www.scb.se/hitta-statistik/sverige-i-siffror/utbildning-jobb-och-pengar/utbildningsnivan-i-sverige/) 
+<br>
+Röstberättigade efter ålder: [https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__ME__ME0105__ME0105C/ME0105T28/table/tableViewLayout1/](https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__ME__ME0105__ME0105C/ME0105T28/table/tableViewLayout1/) 
+<br>
+Röstberättigade efter utbildningsnivå: [https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__ME__ME0105__ME0105C/ME0105T28/table/tableViewLayout1/](https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__ME__ME0105__ME0105C/ME0105T28/table/tableViewLayout1/) 
+<br>
+Befolkningsstatistik: [https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__BE__BE0101__BE0101A/BefolkManad/](https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__BE__BE0101__BE0101A/BefolkManad/)
+
+Arbetsförmedlingen är en svensk statlig myndighet som hjälper människor att hitta jobb och arbetsgivare att hitta personal.
 De erbjuder stöd till arbetssökande, till exempel hjälp med jobbsökande, praktikplatser och utbildningar, samt stöd till arbetsgivare vid rekrytering.
+
 <br>
-  https://arbetsformedlingen.se/statistik/sok-statistik/tidigare-statistik-tidsserier 
+
+Arbetslöshet: [https://arbetsformedlingen.se/statistik/sok-statistik/tidigare-statistik-tidsserier](https://arbetsformedlingen.se/statistik/sok-statistik/tidigare-statistik-tidsserier) 
 `);
