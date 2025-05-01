41151d52bf8efd043ff773176b3ae860264d63dd
Lompish <linda.hiermann.data24sto@edu.tucsweden.se>
2025-05-01 12:14:57 +0200

    added sources.js to menu

--- js/_menu.js
+++ js/_menu.js
@@ -3,5 +3,6 @@ createMenu('Inkomstnivå och valresultat i svenska kommuner', [
   { name: 'Linda', script: 'page4.js' },
   { name: 'Malin', script: 'page2.js' },
   { name: 'Thomas', script: 'page3.js' },
-  { name: 'Nadia', script: 'page1.js' }
+  { name: 'Nadia', script: 'page1.js' },
+  { name: 'Källor', script: 'sources.js' }
 ])
